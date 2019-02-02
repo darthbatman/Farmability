f80d2768c1ea197ae62ce8f39d54b19ef2b10b57
Rishi Masand <masandrishi@gmail.com>
2019-02-02 14:38:17 -0600

    added getting image for given coordinates

---
+++ src/app.py
@@ -0,0 +1,5 @@
+import map_to_image
+
+
+if __name__ == '__main__':
+    map_to_image.save_image_for_location('field.jpg', 40.2201706, -88.1141121)
---
+++ src/map_to_image.py
@@ -0,0 +1,21 @@
+from PIL import Image
+from google_maps_static_api_request import GoogleMapsStaticAPIRequest
+
+
+def crop_out_google_footer(source_image_path, cropped_image_path):
+    Image.open(source_image_path).convert('RGB').save(source_image_path)
+    image_obj = Image.open(source_image_path)
+    crop_end_x = image_obj.size[0]
+    crop_end_y = image_obj.size[1] - 30
+    cropped_image = image_obj.crop((0, 0, crop_end_x, crop_end_y))
+    cropped_image.save(cropped_image_path)
+
+
+def save_image_for_location(image_path, latitude, longitude):
+    request = GoogleMapsStaticAPIRequest(
+        'satellite',
+        (latitude, longitude),
+        15,
+        (600, 300))
+    request.save_image(image_path)
+    crop_out_google_footer(image_path, image_path)
