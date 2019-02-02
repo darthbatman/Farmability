3fda1f6be0de3a6ad9f182a1e6b3e860ac978e67
Rishi Masand <masandrishi@gmail.com>
2019-02-02 16:26:31 -0600

    cleaned up dominant_image_color.py

--- src/dominant_image_color.py
+++ src/dominant_image_color.py
@@ -14,11 +14,13 @@ def dominant_color(image_path):
     """
     image = Image.open(image_path)
     image = image.resize((150, 150))
-    ar = np.asarray(image)
-    shape = ar.shape
-    ar = ar.reshape(scipy.product(shape[:2]), shape[2]).astype(float)
-    codes, dist = scipy.cluster.vq.kmeans(ar, NUM_CLUSTERS)
-    vecs, dist = scipy.cluster.vq.vq(ar, codes)
+    image_array = np.asarray(image)
+    image_shape = image_array.shape
+    image_array = image_array.reshsape(
+        scipy.product(image_shape[:2]),
+        image_shape[2]).astype(float)
+    codes, dist = scipy.cluster.vq.kmeans(image_array, NUM_CLUSTERS)
+    vecs, dist = scipy.cluster.vq.vq(image_array, codes)
     counts, bins = scipy.histogram(vecs, len(codes))
     index_max = scipy.argmax(counts)
     peak = codes[index_max]
