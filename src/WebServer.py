2e474af3233fe19cd724d87a71b193972640407e
Abhinav Tekulapally <abhitekula@users.noreply.github.com>
2019-02-02 18:15:18 -0600

    Moved server to src directory

--- src/WebServer.py
+++ src/WebServer.py
@@ -7,9 +7,11 @@ app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = 'assets'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -30,7 +32,7 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    
+
     return render_template('index.html')
 
 
@@ -38,15 +40,17 @@ def upload_file():
 def uploaded_file(filename):
     filename = app.config['UPLOAD_FOLDER'] + '/' + filename
     return render_template('uploaded_image.html', filename=filename)
-    
+
+
 @app.route('/assets/<filename>')
 def send_assets(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+
 @app.route('/js/<path:path>')
 def send_js(path):
     return send_from_directory('js', path)
-    
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
