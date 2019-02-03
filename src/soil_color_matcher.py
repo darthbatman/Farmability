7b077ecf96a4d70f2175035a958e224de895b9b5
Rishi Masand <masandrishi@gmail.com>
2019-02-03 01:11:54 -0600

    soil color matching script

---
+++ src/soil_color_matcher.py
@@ -0,0 +1,8 @@
+import sys
+import soil_color
+
+if __name__ == '__main__':
+    soil_match = soil_color.find_closest_soil_match_by_color(
+        (int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3])),
+        '../soil.json')
+    print(soil_match)
